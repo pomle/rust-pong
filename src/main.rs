2296aeff1324c8a8e141ae78bfd51d1c5f43929b
Pontus Persson <pom@spotify.com>
2016-04-01 22:05:28 +0200

    Oscillating triangle.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,80 @@
 #[macro_use]
 extern crate glium;
+extern crate time;
 
 fn main() {
-  use std::time::Duration;
   use glium::DisplayBuild;
   use glium::Surface;
 
-
-  let wait = Duration::from_millis(16);
+  #[derive(Copy, Clone)]
+  struct Vertex {
+      position: [f32; 2],
+  }
 
   let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
 
+  implement_vertex!(Vertex, position);
+
+  let shape = vec![
+    Vertex { position: [-0.5, -0.5] },
+    Vertex { position: [ 0.5,  0.5] },
+    Vertex { position: [ 0.5, -0.5] },
+  ];
+
+  let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+  let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+
+  let vertex_shader_src = r#"
+      #version 140
+      uniform float t;
+      in vec2 position;
+      void main() {
+          vec2 pos = position;
+          pos.x += t;
+          gl_Position = vec4(pos, 0.0, 1.0);
+      }
+  "#;
+
+  let fragment_shader_src = r#"
+      #version 140
+      out vec4 color;
+      void main() {
+          color = vec4(1.0, 0.0, 0.0, 1.0);
+      }
+  "#;
+
+  let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+  let mut dt: f64;
+  let mut t: f64 = timestamp();
+  let mut tl = t;
+  let mut elapsed: f64 = 0.0;
+
   loop {
+    t = timestamp();
+    dt = t - tl;
+
     let mut target = display.draw();
     target = draw(target);
+    target.draw(&vertex_buffer,
+                &indices,
+                &program,
+                &uniform! {
+                  t: elapsed.sin() as f32,
+                },
+                &Default::default()).unwrap();
     target.finish().unwrap();
-    std::thread::sleep(wait);
+
+    tl = t;
+    elapsed += dt;
   }
+}
+
+fn timestamp() -> f64 {
+  let timespec = time::get_time();
+  let mills: f64 = timespec.sec as f64 + (timespec.nsec as f64 / 1000.0 / 1000.0 / 1000.0);
+  mills
 }
 
 fn draw(mut target: glium::Frame) -> glium::Frame {
