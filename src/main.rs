a6e3fe2667c414b616a1667f199f2f5a8d0eba57
Pontus Persson <pontus.persson@einride.tech>
2020-02-21 15:11:41 +0100

    Somewhat pongified

--- src/main.rs
+++ src/main.rs
@@ -10,39 +10,68 @@ use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 
 pub struct Ball {
+    size: f64,
     pos: [f64;2],
     vel: [f64;2]
 }
 
+pub struct Paddle {
+    size: [f64;2],
+    pos: [f64;2],
+}
+
+fn make_pad(pos: [f64;2]) -> Paddle {
+    Paddle {
+        size: [20.0, 80.0],
+        pos,
+    }
+}
+
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64,  // Rotation for the square.
-    ball: Ball,
 
+    ball: Ball,
+    paddles: [Paddle;2],
+    court: [f64;2],
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
+        dbg!(args);
         use graphics::rectangle;
         use graphics::clear;
 
         const BACKGROUND: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
         const FOREGROUND: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
 
-        let ball = rectangle::square(self.ball.pos[0], self.ball.pos[1], 50.0);
+        let rects = [
+            rectangle::square(self.ball.pos[0], self.ball.pos[1], self.ball.size),
+            [self.paddles[0].pos[0], self.paddles[0].pos[1],
+                self.paddles[0].size[0], self.paddles[0].size[1]],
+            [self.paddles[1].pos[0], self.paddles[1].pos[1],
+                self.paddles[1].size[0], self.paddles[1].size[1]],
+        ];
 
         self.gl.draw(args.viewport(), |context, gl| {
             // Clear the screen.
             clear(BACKGROUND, gl);
 
             // Draw a box rotating around the middle of the screen.
-            rectangle(FOREGROUND, ball, context.transform, gl);
+            for rect in &rects {
+                rectangle(FOREGROUND, *rect, context.transform, gl);
+            }
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        self.ball.pos[0] += self.ball.vel[0];
-        self.ball.pos[1] += self.ball.vel[1];
+        self.ball.pos[0] += self.ball.vel[0] * args.dt;
+        self.ball.pos[1] += self.ball.vel[1] * args.dt;
+
+        if
+            (self.ball.vel[1] < 0.0 && self.ball.pos[1] < 0.0) ||
+            (self.ball.vel[1] > 0.0 && self.ball.pos[1] > self.court[1] - self.ball.size) {
+                self.ball.vel[1] *= -1.0;
+        }
     }
 }
 
@@ -57,14 +86,20 @@ fn main() {
         .build()
         .unwrap();
 
+    let court_size = [600.0, 300.0];
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0,
         ball: Ball {
+            size: 50.0,
             pos: [0.0, 0.0],
-            vel: [1.0, 1.0],
+            vel: [100.0, 100.0],
         },
+        paddles: [
+            make_pad([20.0, court_size[1] / 2.0]),
+            make_pad([court_size[0] - 20.0, court_size[1] / 2.0]),
+        ],
+        court: court_size,
     };
 
     let mut events = Events::new(EventSettings::new());
