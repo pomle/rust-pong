f64db25072fb513f1ec139ead52a90f3d71fc48b
Pontus Persson <pontus.persson@einride.tech>
2020-02-21 14:04:20 +0100

    Bigger screen

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ fn main() {
     let opengl = OpenGL::V3_2;
 
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("spinning-square", [200, 200])
+    let mut window: Window = WindowSettings::new("spinning-square", [600, 300])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
